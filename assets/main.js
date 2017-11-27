ab98f200b347cf039f882e13ba0d1fe9fc4ea0b2
Larry Owen <charv13@gmail.com>
2017-11-27 10:15:43 -0600

    First commit

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+<script type="text/javascript" src="https://assets.zendesk.com/apps/sdk/2.0/zaf_sdk.js"></script>
+var client = ZAFClient.init();
+
+console.log("connected");
+
+///<script type="text/javascript" src="https://assets.zendesk.com/apps/sdk/2.0/zaf_sdk.js"></script>
+ // <script>
+   // console.log("connected");
+   // var ticket = document.getElementById("ticket_info");
+    // Initialise the Zendesk JavaScript API client
+    // https://developer.zendesk.com/apps/docs/apps-v2
+   // var client = ZAFClient.init();
+   // client.invoke('resize', { width: '100%', height: '200px' });
+
+    //client.get('ticket.requester.name').then(function(data) {
+   // console.log(data); // { "ticket.requester.name": "Mikkel Svane" }
+
+//});
